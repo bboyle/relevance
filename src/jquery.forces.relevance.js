19e4961c78efaaa12181df98f4153d40ff3cbaf7
Ben Boyle <benjamins.boyle@gmail.com>
2012-11-10 14:24:17 +1000

    support punctuation in instructions. Closes #11
    - values in instructions must end with ', " or ’ character
    - fixed regexp handling
    - moved jslint config into source js header
    - allow ^ regexp jslint toggled on

--- src/jquery.forces.relevance.js
+++ src/jquery.forces.relevance.js
@@ -6,6 +6,8 @@
 
 */
 
+/*jslint browser:true,white:true,plusplus:true,regexp:true*/
+/*global jQuery*/
 if ( jQuery !== 'undefined' ) {
 (function( $ ) {
 	'use strict';
@@ -213,7 +215,7 @@ if ( jQuery !== 'undefined' ) {
 
 			this.find( options.instructionSelector ).each(function() {
 				var $this = $( this ),
-					value = $this.text().replace( /^[\S\s]*chose \W([\w\s]+)\W above[\S\s]*$/, '$1' ),
+					value = $this.text().replace( /^[\S\s]*chose \W([^'"’]+)['"’] above[\S\s]*$/, '$1' ),
 					question = $this.closest( options.questionSelector ),
 					toggle = question.prevAll( options.questionSelector ),
 					i, answers,
