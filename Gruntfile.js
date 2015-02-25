1463864c6736ec4553c91631fa4e37d90805b168
Ben Boyle <benjamins.boyle@gmail.com>
2015-02-25 22:33:14 +1000

    load grunt-saucelabs plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,6 +171,7 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+	grunt.loadNpmTasks( 'grunt-saucelabs' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
